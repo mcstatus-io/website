14cc95f50de308989f0ea3343ef9cdde99b2a5c7
Jacob Gunther <jacobgunther03+gitlab@gmail.com>
2021-11-20 03:55:33 -0600

    Style changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Header() {
 	return (
 		<div className="container main-container pt-6">
-			<img src="/img/stone.png" width="64" height="64" className="is-pulled-left mr-4 mt-4" />
+			<img src="/img/stone.png" width="64" height="64" alt="Stone block" className="is-pulled-left mr-4 mt-4" />
 			<Link href="/">
 				<a>
 					<h1 className="title is-1">mcstatus.io</h1>
