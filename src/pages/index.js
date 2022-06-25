d263fb0ce7176907aa11a1d0383045378dc2b8e7
Jacob Gunther <jacobgunther03+gitlab@gmail.com>
2022-06-25 15:27:57 -0500

    Use formik for forms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,30 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Script from 'next/script';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import { exampleServers } from '../assets/servers';
 
 export default function Home({ servers }) {
-	const inputElem = useRef(null);
-	const bedrockElem = useRef(null);
 	const { push, pathname } = useRouter();
-	const [error, setError] = useState(false);
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-
-		if (!inputElem || !inputElem.current || inputElem.current.value.length < 1) return;
-
-		push(`/status/${bedrockElem && bedrockElem.current && bedrockElem.current.checked ? 'bedrock' : 'java'}/${inputElem.current.value.toLowerCase()}`);
-	};
-
-	const onChange = () => {
-		if (!inputElem || !inputElem.current) return;
-
-		setError(inputElem.current.value.length < 1);
-	};
+	const form = useFormik({
+		initialValues: {
+			host: '',
+			bedrock: false
+		},
+		validationSchema: Yup
+			.object()
+			.shape({
+				host: Yup.string().min(1).matches(/^[A-Za-z0-9-_]+(\.[A-Za-z0-9-_]+)*(:\d{1,5})?$/).required(),
+				bedrock: Yup.boolean().required()
+			})
+			.required(),
+		onSubmit: ({ host, bedrock }) => push(`/status/${bedrock ? 'bedrock' : 'java'}/${host.toLowerCase()}`)
+	});
 
 	return (
 		<>
@@ -42,21 +42,21 @@ export default function Home({ servers }) {
 			</Head>
 			<div className="container">
 				<h1 className="title">Minecraft Server Status</h1>
-				<form onSubmit={onSubmit} className="mb-5">
+				<form onSubmit={form.handleSubmit} className="mb-5">
 					<div className="columns">
 						<div className="column is-flex-grow-1">
 							<div className="field">
 								<div className="control is-fullwidth">
-									<input type="text" className={`input ${error ? 'is-danger' : ''}`} placeholder="play.hypixel.net OR play.hypixel.net:25565" spellCheck="false" autoComplete="false" onChange={onChange} ref={inputElem} />
+									<input type="text" className={`input ${form.errors.host ? 'is-danger' : ''}`} id="host" placeholder="play.hypixel.net OR play.hypixel.net:25565" value={form.values.host} spellCheck="false" autoComplete="false" onChange={form.handleChange} onBlur={form.handleBlur} />
 								</div>
 							</div>
 							<label className="checkbox">
-								<input type="checkbox" className="mr-2" ref={bedrockElem} />
+								<input type="checkbox" className="mr-2" id="bedrock" checked={form.values.bedrock} onChange={form.handleChange} />
 								<span>Bedrock server</span>
 							</label>
 						</div>
 						<div className="column is-flex-grow-0">
-							<button type="submit" className="button is-fullwidth is-link">Submit</button>
+							<button type="submit" className="button is-fullwidth is-link" disabled={!form.isValid}>Submit</button>
 						</div>
 					</div>
 				</form>
--- src/pages/status/bedrock/[address].js
+++ src/pages/status/bedrock/[address].js
@@ -1,41 +1,29 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import humanizeDuration from 'humanize-duration';
 
 export default function Status({ address, result, error, cache }) {
-	const inputElem = useRef(null);
-	const bedrockElem = useRef(null);
 	const { push, pathname } = useRouter();
-	const [inputError, setInputError] = useState(false);
 	const [showDebug, setShowDebug] = useState(false);
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-
-		if (
-			!inputElem
-			|| !inputElem.current
-			|| inputElem.current.value.length < 1
-			|| !bedrockElem
-			|| !bedrockElem.current
-			|| (
-				inputElem.current.value === address
-				&& bedrockElem.current.checked
-			)
-		) return;
-
-		setInputError(null);
-
-		push(`/status/${bedrockElem && bedrockElem.current && bedrockElem.current.checked ? 'bedrock' : 'java'}/${inputElem.current.value.toLowerCase()}`);
-	};
-
-	const onChange = () => {
-		if (!inputElem || !inputElem.current) return;
-
-		setInputError(inputElem.current.value.length < 1);
-	};
+	const form = useFormik({
+		initialValues: {
+			host: address,
+			bedrock: true
+		},
+		validationSchema: Yup
+			.object()
+			.shape({
+				host: Yup.string().min(1).matches(/^[A-Za-z0-9-_]+(\.[A-Za-z0-9-_]+)*(:\d{1,5})?$/).required(),
+				bedrock: Yup.boolean().required()
+			})
+			.required(),
+		onSubmit: ({ host, bedrock }) => push(`/status/${bedrock ? 'bedrock' : 'java'}/${host.toLowerCase()}`)
+	});
 
 	return (
 		<>
@@ -53,21 +41,21 @@ export default function Status({ address, result, error, cache }) {
 			</Head>
 			<div className="container">
 				<h1 className="title">Minecraft Server Status</h1>
-				<form onSubmit={onSubmit} className="mb-5" spellCheck="false" autoComplete="false">
+				<form onSubmit={form.handleSubmit} className="mb-5">
 					<div className="columns">
 						<div className="column is-flex-grow-1">
 							<div className="field">
 								<div className="control is-fullwidth">
-									<input type="text" className={`input ${inputError ? 'is-danger' : ''}`} placeholder="play.hypixel.net OR play.hypixel.net:25565" defaultValue={address} spellCheck="false" autoComplete="false" onChange={onChange} ref={inputElem} />
+									<input type="text" className={`input ${form.errors.host ? 'is-danger' : ''}`} id="host" placeholder="play.hypixel.net OR play.hypixel.net:25565" value={form.values.host} spellCheck="false" autoComplete="false" onChange={form.handleChange} onBlur={form.handleBlur} />
 								</div>
 							</div>
 							<label className="checkbox">
-								<input type="checkbox" className="mr-2" defaultChecked={true} ref={bedrockElem} />
+								<input type="checkbox" className="mr-2" id="bedrock" checked={form.values.bedrock} onChange={form.handleChange} />
 								<span>Bedrock server</span>
 							</label>
 						</div>
 						<div className="column is-flex-grow-0">
-							<button type="submit" className="button is-link">Submit</button>
+							<button type="submit" className="button is-fullwidth is-link" disabled={!form.isValid}>Submit</button>
 						</div>
 					</div>
 				</form>
