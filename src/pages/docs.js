b5e0def83eb3a3e25482ee48efc48548380c7900
Jacob Gunther <jacobgunther03@gmail.com>
2022-06-27 20:14:53 -0500

    Add favicon route to docs

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -4,6 +4,7 @@ import Script from 'next/script';
 import Highlight from 'react-highlight';
 import javaExample from '../assets/response/java.jsonc';
 import bedrockExample from '../assets/response/bedrock.jsonc';
+import sampleIcon from '../assets/response/icon.png';
 
 const revisions = [
 	{
@@ -15,12 +16,15 @@ const revisions = [
 				<p>mcstatus.io believes that anybody should be able to retrieve the status of any Minecraft server using a simple and efficient manner. This is why we prioritize a detailed and continuously updated documentation on how to interact with our service from yours. Please make sure to read this documentation thoroughly to prevent any errors that may be a mistake on your part.</p>
 				<p>All methods documented are using the REST API, which is supported in all major programming languages and browsers. Body data sent and received from/to the server are using JSON formatting for standardization reasons. You should familiarize yourself with this data encoding before attempting to use our service. If you have any questions, concerns or encounter any problems after attempting a solution, please feel free to contact us by sending an email to <a href="mailto:api@mcstatus.io">api@mcstatus.io</a>.</p>
 				<p>All server statuses are cached for up to 10 minutes from the previous network fetch. You can determine if a status was fetched from cache by using the <code>X-Cache-Hit</code> header returned from the server after the request. If the cache was used, there will also be a <code>X-Cache-Time-Remaining</code> header that will contain the amount of seconds remaining until the cache expires.</p>
-				<h2 className="title is-header mt-6">Java Status</h2>
+				<h2 className="title is-header mt-6 mb-3">Java Status</h2>
 				<p><span className="tag is-success">GET</span> <code>https://api.mcstatus.io/v1/status/java/&lt;address&gt;</code></p>
 				<Highlight className="language-json p-3">{javaExample}</Highlight>
-				<h2 className="title is-header mt-6">Bedrock Status</h2>
+				<h2 className="title is-header mt-6 mb-3">Bedrock Status</h2>
 				<p><span className="tag is-success">GET</span> <code>https://api.mcstatus.io/v1/status/bedrock/&lt;address&gt;</code></p>
 				<Highlight className="language-json p-3">{bedrockExample}</Highlight>
+				<h2 className="title is-header mt-6 mb-3">Favicon</h2>
+				<p><span className="tag is-success">GET</span> <code>https://api.mcstatus.io/v1/favicon/&lt;address&gt;/&lt;filename&gt;.png</code></p>
+				<img src={sampleIcon.src} alt="Sample icon of Hypixel" width="128" height="128" />
 			</>
 		)
 	}
