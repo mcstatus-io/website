82d705c9db937982092daa509b55b24b9149ab19
Jacob Gunther <jacobgunther03+github@gmail.com>
2022-08-25 01:39:23 -0500

    Change primary font to Ubuntu and self host

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,35 +16,35 @@ export default function Header() {
 		<div className="header is-align-items-center is-justify-content-space-between">
 			<Link href="/">
 				<a className="is-flex is-align-items-center">
-					<Image src={icon} className="is-align-middle" alt="Icon" width="64" height="64" />
-					<span className="title has-text-white is-align-middle ml-3">mcstatus.io</span>
+					<Image src={icon} className="is-align-middle" alt="Icon" width="64" height="64" priority />
+					<span className="title has-text-white has-text-weight-medium is-align-middle ml-3">mcstatus.io</span>
 				</a>
 			</Link>
 			<div className="buttons">
 				<Link href="/">
 					<a className={`button ${pathname === '/' ? 'is-link' : 'is-dark-light'} px-4`}>
-						<Image src={homeIcon} alt="Home icon" className="nav-icon" />
+						<Image src={homeIcon} alt="Home icon" className="nav-icon" priority />
 						<span>Home</span>
 					</a>
 				</Link>
 				<Link href="/docs/v2">
 					<a className={`button ${pathname.startsWith('/docs') ? 'is-link' : 'is-dark-light'} px-4`}>
-						<Image src={linkIcon} alt="Link icon" className="nav-icon" />
+						<Image src={linkIcon} alt="Link icon" className="nav-icon" priority />
 						<span>API</span>
 					</a>
 				</Link>
 				<Link href="/about">
 					<a className={`button ${pathname === '/about' ? 'is-link' : 'is-dark-light'} px-4`}>
-						<Image src={infoIcon} alt="Info icon" className="nav-icon" />
+						<Image src={infoIcon} alt="Info icon" className="nav-icon" priority />
 						<span>About</span>
 					</a>
 				</Link>
 				<a href="https://uptime.mcstatus.io" className="button is-dark-light px-4">
-					<Image src={calendarIcon} alt="Calendar icon" className="nav-icon" />
+					<Image src={calendarIcon} alt="Calendar icon" className="nav-icon" priority />
 					<span>Uptime</span>
 				</a>
 				<a href="https://github.com/mcstatus-io" className="button is-dark-light px-4">
-					<Image src={githubIcon} alt="GitHub icon" className="nav-icon" />
+					<Image src={githubIcon} alt="GitHub icon" className="nav-icon" priority />
 					<span>GitHub</span>
 				</a>
 			</div>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,18 +12,15 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-					<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400&family=Poppins:wght@300;400;600&amp;display=swap" rel="stylesheet" />
 					<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 					<link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
 					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 					<link rel="manifest" href="/site.webmanifest" />
 					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2f2f2f" />
-					<meta name="msapplication-TileColor" content="#ffffff" />
+					<meta name="msapplication-TileColor" content="#232323" />
 					<meta name="msapplication-TileImage" content="/mstile-144x144.png" />
-					<meta name="theme-color" content="#ffffff" />
+					<meta name="theme-color" content="#232323" />
 				</Head>
 				<body>
 					<Main />
