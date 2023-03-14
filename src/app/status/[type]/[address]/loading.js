2bcac3e044db176f6e693cd0c93c44d9388d8a10
Jacob Gunther <jacobgunther03+github@gmail.com>
2023-03-13 19:07:09 -0500

    Add aria-busy attributes to status pages

--- src/app/status/[type]/[address]/loading.js
+++ src/app/status/[type]/[address]/loading.js
@@ -1,6 +1,6 @@
 export default function Loading() {
 	return (
-		<section>
+		<section aria-busy="true" aria-live="assertive" aria-atomic="true">
 			<div className="flex gap-3 px-5 py-4 rounded mt-4 box">
 				<div className="w-1/4">
 					<div className="block rounded bg-neutral-300 dark:bg-neutral-700 opacity-70 h-12 w-full mb-3" />
