56f570e75d9bcac6e7efecc03f10aebf5e781818
Jacob Gunther <jacobgunther03+github@gmail.com>
2023-03-23 19:05:16 -0500

    Remove offline page

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,16 +1,10 @@
 const cacheName = 'mcstatus-v2';
 
-const OFFLINE_URL = 'https://mcstatus.io/offline';
-
 self.addEventListener('install', (event) => {
-	event.waitUntil(caches.open(cacheName).then((cache) => fetch(OFFLINE_URL).then((result) => cache.put(OFFLINE_URL, result))));
+	event.waitUntil(caches.open(cacheName));
 });
 
 self.addEventListener('fetch', (event) => {
-	if (event.request.mode === 'navigate' && !navigator.onLine) {
-		return event.respondWith(caches.open(cacheName).then((cache) => cache.match(OFFLINE_URL)));
-	}
-
 	event.respondWith(caches.open(cacheName).then(async (cache) => {
 		return fetch(event.request).then((fetchedResponse) => {
 			if (/^https?/.test(new URL(event.request.url).protocol)) {
