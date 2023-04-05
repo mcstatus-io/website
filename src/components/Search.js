d6907be4111328cc50c07494104a86928a9174d6
Jacob Gunther <jacobgunther03+github@gmail.com>
2023-04-05 17:29:34 -0500

    Responsiveness improvements, code cleanup

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,21 +5,23 @@ import { useRouter } from 'next/navigation';
 
 const validHostRegEx = /^[A-Za-z0-9-_]+(\.[A-Za-z0-9-_]+)*(:\d{1,5})?$/;
 
+const isValid = (state) => state.host.length > 0 && validHostRegEx.test(state.host) && ['java', 'bedrock'].includes(state.type);
+
 const reducer = (state, action) => {
 	switch (action.type) {
 		case 'SET_HOST':
-			return { ...state, host: action.value, isValid: validHostRegEx.test(action.value) };
+			return { ...state, host: action.value };
 		case 'SET_TYPE':
-			return { ...state, type: action.value, isValid: validHostRegEx.test(action.value) };
+			return { ...state, type: action.value };
 		default:
 			return state;
 	}
 };
 
-export default function Search({ host, type, className }) {
+export default function Search({ host = '', type = 'java', className }) {
 	const { push } = useRouter();
 
-	const [data, dispatch] = useReducer(reducer, { host: host ?? '', type: type ?? 'java', isValid: false });
+	const [data, dispatch] = useReducer(reducer, { host, type });
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
@@ -28,22 +30,22 @@ export default function Search({ host, type, className }) {
 	};
 
 	return (
-		<form className={`block md:flex items-center gap-5 ${className ?? ''}`} onSubmit={handleSubmit}>
-			<div className="flex items-center gap-5 grow">
-				<div className="mb-3 md:mb-0">
+		<form className={`flex flex-col md:flex-row items-center gap-3 ${className ?? ''}`} onSubmit={handleSubmit}>
+			<div className="flex flex-col sm:flex-row items-center gap-3 grow w-full md:w-auto">
+				<div className="w-full sm:w-auto">
 					<label className="sr-only" htmlFor="type">Server Type</label>
-					<select className="select md:w-auto" id="type" defaultValue={type} onChange={(event) => dispatch({ type: 'SET_TYPE', value: event.target.value })}>
+					<select className="select w-full md:w-auto" id="type" defaultValue={type} onChange={(event) => dispatch({ type: 'SET_TYPE', value: event.target.value })}>
 						<option value="java" className="text-neutral-800">Java Edition</option>
 						<option value="bedrock" className="text-neutral-800">Bedrock Edition</option>
 					</select>
 				</div>
-				<div className="grow mb-3 md:mb-0">
+				<div className="grow w-full sm:w-auto">
 					<label className="sr-only" htmlFor="host">Host</label>
-					<input type="text" className="input text-center md:text-left" id="host" placeholder="play.hypixel.net" defaultValue={host} onChange={(event) => dispatch({ type: 'SET_HOST', value: event.target.value })} />
+					<input type="text" className="input text-center md:text-left w-full" id="host" placeholder="play.hypixel.net" defaultValue={host} onChange={(event) => dispatch({ type: 'SET_HOST', value: event.target.value })} />
 				</div>
 			</div>
-			<div>
-				<button type="submit" className="button block w-full md:w-auto" disabled={!data.isValid || (data.type === type && data.host === host)}>
+			<div className="w-full md:w-auto">
+				<button type="submit" className="button block w-full" disabled={!isValid(data) || (data.type === type && data.host === host)}>
 					Submit
 				</button>
 			</div>
