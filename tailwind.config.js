2cae2aecf2c873499f738bf917be62e6e63d86e9
Jacob Gunther <jacobgunther03+github@gmail.com>
2023-05-04 18:54:36 -0500

    Remove unused Tailwind content matches

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
 	important: true,
 	content: [
 		'./src/app/**/*.{js,ts,jsx,tsx}',
-		'./src/pages/**/*.{js,ts,jsx,tsx}',
-		'./src/components/**/*.{js,ts,jsx,tsx}',
-		'./src/layouts/**/*.{js,ts,jsx,tsx}'
+		'./src/components/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
 		extend: {},
