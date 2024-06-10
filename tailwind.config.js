9f842a74b9507d710b2bac29ea40f57a696a7971
Jacob Gunther <jacobgunther03+github@gmail.com>
2024-06-10 09:42:55 -0500

    Fix build error with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    important: true,
     content: [
         './src/app/**/*.{js,ts,jsx,tsx}',
         './src/components/**/*.{js,ts,jsx,tsx}'
